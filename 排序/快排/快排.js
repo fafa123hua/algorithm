17cd3f54a4dcb0c6c0ab39fe25c7f06c830dc07b
fafa123hua <65300014+fafa123hua@users.noreply.github.com>
2021-10-06 23:54:09 +0800

    feat:sort

---
+++ 排序/快排/快排.js
@@ -0,0 +1,21 @@
+// quick sort
+// 快排在最糟糕得情况下时间复杂度是O(n²)，平均的复杂度是O(nlogn)，
+var nums = [9, 10, 1, 2, 8, 4, 5]
+var quickSort = function (nums) {
+  if (nums.length <= 1) {
+    return nums
+  }
+  let centre = Math.floor(nums.length / 2) // 取中间坐标
+  let pivot = nums.splice(centre, 1)[0] // 取对应数值
+  let left = [],
+    right = []
+  nums.forEach((element, i) => {
+    if (element < pivot) {
+      left.push(element)
+    } else {
+      right.push(element)
+    }
+  })
+  return quickSort(left).concat(pivot, quickSort(right)) // 递归
+}
+console.log(quickSort(nums))
