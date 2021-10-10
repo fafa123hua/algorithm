893dc72691f825038493f10781e4caa9cb2781cb
fafa123hua <65300014+fafa123hua@users.noreply.github.com>
2021-10-10 23:54:30 +0800

    feat:rqsort

---
+++ 排序/快排/2随机普通快排.js
@@ -0,0 +1,24 @@
+// rquick sort
+// 平均意义上，对于任何数组（包括渐进有序数组），快排遇到最差情况的概率将大大降低
+var nums = [9, 10, 1, 1, 2, 8, 4, 5]
+var rquickSort = function (nums) {
+  if (nums.length <= 1) {
+    return nums
+  }
+  let centre = Math.floor(Math.random() * Math.floor(nums.length - 1)) // 取随机坐标
+  let pivot = nums.splice(centre, 1)[0] // 取对应数值
+  let left = [],
+    right = []
+  nums.forEach((element) => {
+    // '<'和'>'决定升序降序
+    if (element < pivot) {
+      left.push(element)
+    } else {
+      right.push(element)
+    }
+  })
+  console.log(left, pivot, right)
+
+  return rquickSort(left).concat(pivot, rquickSort(right)) // 递归
+}
+console.log(rquickSort(nums))
